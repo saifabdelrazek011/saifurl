5a484f6f9c6a588da43ed594f40bc13f062b51ff
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-07-09 18:55:18 +0300

    feat: Integrate Vercel Analytics for improved tracking and analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import "./index.css";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import { DashboardProvider } from "./contexts/DashboardContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DashboardProvider>
       <App />
+      <Analytics />
     </DashboardProvider>
   </StrictMode>
 );
