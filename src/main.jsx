a32d3adc8f7604562e2707cfb85171dbc2f962e9
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-07-11 10:17:29 +0300

    Remove unused Vercel Analytics import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import "./index.css";
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import { DashboardProvider } from "./contexts/DashboardContext.jsx";
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
